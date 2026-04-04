2eb9d341f1dc870c0eb87e24b8c76b99212506c0
Ali Andac Erdas <aliandacerdass@gmail.com>
2026-04-04 20:57:47 +0300

    Lesson1.jova created also code wrote which is calculate the area of the circle

---
+++ SchoolLesson1.java
@@ -0,0 +1,12 @@
+public class SchoolLesson1 {
+    public static void main(){
+        System.out.println("Hello world");
+
+        double radius,area;
+        double pi = 3.14;
+        // Assing a radius 
+        radius = 20 ;
+        area = (radius*radius*pi);
+        System.out.println("The area of the circle is: "+ area);
+    }
+}
