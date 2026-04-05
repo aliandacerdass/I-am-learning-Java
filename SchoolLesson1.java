504f8847752c046c569a0895713cbcc858576c28
Ali Andac Erdas <aliandacerdass@gmail.com>
2026-04-05 10:46:49 +0300

    first Lesson is finished.

--- SchoolLesson1.java
+++ SchoolLesson1.java
@@ -1,12 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner; // Bunu ekledik! Locale, bölge ve dil ayarlarını kontrol eder.
+
 public class SchoolLesson1 {
-    public static void main(){
+    public static void main(String[] args){ // Buraya da değineceğim
         System.out.println("Hello world");
 
         double radius,area;
         double pi = 3.14;
-        // Assing a radius 
+        
         radius = 20 ;
         area = (radius*radius*pi);
         System.out.println("The area of the circle is: "+ area);
+    
+        Scanner input = new Scanner(System.in);
+        
+        // Scanner'a Amerikan (US) standartlarını kullanmasını, 
+        // yani noktayı ondalık ayracı olarak görmesini söyledik:
+        input.useLocale(Locale.US); 
+        
+        System.out.println("please enter a double");
+        double d = input.nextDouble();
+        System.out.printf("girilen sayi: %.6f",d);
     }
 }
